95a58273c93d44c729cc55fd2ef1df005c06bc9f
Neil Beesley <73969989+neilbeesley@users.noreply.github.com>
2025-04-04 21:54:38 -0500

    Create waiter.sh
    Added
    
    Signed-off-by: Neil Beesley <73969989+neilbeesley@users.noreply.github.com>

---
+++ NewBuild/waiter.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+echo "Arg0: $0"; # script name
+echo "Arg1: $1"; # time period to wait for job to complete
+echo "Arg2: $2"; # lock signal filename
+
+timeout="$1";
+
+if [[ "$timeout" =~ ^-?[0-9]+(\.[0-9]+)?$ ]]; then
+  echo "numeric timeout: $timeout";
+else
+  if [[ "$timeout" = "" ]]; then
+    echo "timeout value blank, so use default (300)";
+    timeout=300;
+  else
+    echo "timeout value not numeric: $input";
+    exit 1; # It's not numeric (false)
+  fi
+fi
+echo "timeout: $timeout";
+
+Filel="$2";
+if [[ "$Filel" = "" ]]; then
+  Filel="lockdone";
+fi
+
+current_time_ms=$(date +"%H:%M:%S");
+echo "Job sibmitted at: $current_time_ms";
+
+# timeout=$(input); # Timeout in seconds
+elapsed_time=0;
+
+# Filel="lockdone";
+echo "Filel: $Filel";
+
+while [ ! ]; do
+  # test for existence of done lock
+  ls $Filel;
+  status=$?;
+  echo "status: $status";
+  if [ $status -eq 0 ]; then
+    current_time_ms=$(date +"%H:%M:%S");
+    echo "Job completed: $current_time_ms";
+    echo "Done signal lock identified and removed";
+    rm $Filel;
+    exit 0;
+  fi
+
+  # check for expiration of waiting period
+  echo "elapsed_time: $elapsed_time";
+  elapsed_time=$((elapsed_time+5));
+  if [ "$elapsed_time" -gt "$timeout" ]; then
+    echo "Timeout reached without determining job completion";
+    exit 1;
+  fi
+  sleep 5;
+done
+
+echo "Dont come here!!!!";
