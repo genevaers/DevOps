a74d54d9a9a556f25314c8518fbd928092c97771
ghannington <ghannington2007@gmail.com>
2025-04-30 11:37:57 +0800

    Create ascii version of PE and PEX tables in new TABLE_A directory
    Signed-off-by: ghannington <ghannington2007@gmail.com>

--- FTL/GenBuild.sh
+++ FTL/GenBuild.sh
@@ -4,6 +4,8 @@
 
 main() {
 
+source ./ConcatJCL.sh;
+
 # extract repository names
 PE_REPO=$(basename $GERS_REMOTE_PEB .git);
 echo $PE_REPO
@@ -12,48 +14,35 @@ echo $PEX_REPO
 RCA_REPO=$(basename $GERS_REMOTE_RUN .git);
 echo $RCA_REPO
 # Make ascii copies of the tables
-cp $GERS_GIT_REPO_DIR/$PE_REPO/TABLE/MAC.csv $GERS_GIT_REPO_DIR/$PE_REPO/TABLE/MAC_e.csv;
-cp $GERS_GIT_REPO_DIR/$PE_REPO/TABLE/PGM.csv $GERS_GIT_REPO_DIR/$PE_REPO/TABLE/PGM_e.csv;
-cp $GERS_GIT_REPO_DIR/$PE_REPO/TABLE/tablesPE.csv $GERS_GIT_REPO_DIR/$PE_REPO/TABLE/tablesPE_e.csv;
+mkdir $GERS_GIT_REPO_DIR/$PE_REPO/TABLE_A;
 #
-rm $GERS_GIT_REPO_DIR/$PE_REPO/TABLE/MAC.csv;
-rm $GERS_GIT_REPO_DIR/$PE_REPO/TABLE/PGM.csv;
-rm $GERS_GIT_REPO_DIR/$PE_REPO/TABLE/tablesPE.csv;
-#
-iconv -f IBM-1047 -t ISO8859-1 $GERS_GIT_REPO_DIR/$PE_REPO/TABLE/tablesPE_e.csv  > $GERS_GIT_REPO_DIR/$PE_REPO/TABLE/tablesPE.csv;
-iconv -f IBM-1047 -t ISO8859-1 $GERS_GIT_REPO_DIR/$PE_REPO/TABLE/MAC_e.csv  > $GERS_GIT_REPO_DIR/$PE_REPO/TABLE/MAC.csv;
-iconv -f IBM-1047 -t ISO8859-1 $GERS_GIT_REPO_DIR/$PE_REPO/TABLE/PGM_e.csv  > $GERS_GIT_REPO_DIR/$PE_REPO/TABLE/PGM.csv;
+iconv -f IBM-1047 -t ISO8859-1 $GERS_GIT_REPO_DIR/$PE_REPO/TABLE/tablesPE.csv  > $GERS_GIT_REPO_DIR/$PE_REPO/TABLE_A/tablesPE.csv;
+iconv -f IBM-1047 -t ISO8859-1 $GERS_GIT_REPO_DIR/$PE_REPO/TABLE/MAC.csv  > $GERS_GIT_REPO_DIR/$PE_REPO/TABLE_A/MAC.csv;
+iconv -f IBM-1047 -t ISO8859-1 $GERS_GIT_REPO_DIR/$PE_REPO/TABLE/PGM.csv  > $GERS_GIT_REPO_DIR/$PE_REPO/TABLE_A/PGM.csv;
 #
 # Create copy commands to copy source to data sets - this reads a table in each of the repositories
 #
-java -jar $GERS_RCA_JAR_DIR/ftl2jcl-latest.jar COPYPE $GERS_GIT_REPO_DIR/$PE_REPO/TABLE/tablesPE;
+java -jar $GERS_RCA_JAR_DIR/ftl2jcl-latest.jar COPYPE $GERS_GIT_REPO_DIR/$PE_REPO/TABLE_A/tablesPE;
 exitIfError;
 # Performance Engine extensions required?
 if  [ "$GERS_INCLUDE_PEX" == "Y" ]; then 
 # Make ascii copies of the tables
-  cp $GERS_GIT_REPO_DIR/$PEX_REPO/TABLE/MACRND.csv $GERS_GIT_REPO_DIR/$PEX_REPO/TABLE/MAC_e.csv;
-  cp $GERS_GIT_REPO_DIR/$PEX_REPO/TABLE/PGMRND.csv $GERS_GIT_REPO_DIR/$PEX_REPO/TABLE/PGM_e.csv;
-  cp $GERS_GIT_REPO_DIR/$PEX_REPO/TABLE/tablesPEX.csv $GERS_GIT_REPO_DIR/$PEX_REPO/TABLE/tablesPEX_e.csv;
-#
-  rm $GERS_GIT_REPO_DIR/$PEX_REPO/TABLE/MACRND.csv;
-  rm $GERS_GIT_REPO_DIR/$PEX_REPO/TABLE/PGMRND.csv;
-  rm $GERS_GIT_REPO_DIR/$PEX_REPO/TABLE/tablesPEX.csv;
-#
-  iconv -f IBM-1047 -t ISO8859-1 $GERS_GIT_REPO_DIR/$PEX_REPO/TABLE/tablesPEX_e.csv  > $GERS_GIT_REPO_DIR/$PEX_REPO/TABLE/tablesPEX.csv;
-  iconv -f IBM-1047 -t ISO8859-1 $GERS_GIT_REPO_DIR/$PEX_REPO/TABLE/MAC_e.csv  > $GERS_GIT_REPO_DIR/$PEX_REPO/TABLE/MACRND.csv;
-  iconv -f IBM-1047 -t ISO8859-1 $GERS_GIT_REPO_DIR/$PEX_REPO/TABLE/PGM_e.csv  > $GERS_GIT_REPO_DIR/$PEX_REPO/TABLE/PGMRND.csv;
+  mkdir $GERS_GIT_REPO_DIR/$PEX_REPO/TABLE_A;
+  iconv -f IBM-1047 -t ISO8859-1 $GERS_GIT_REPO_DIR/$PEX_REPO/TABLE/tablesPEX.csv  > $GERS_GIT_REPO_DIR/$PEX_REPO/TABLE_A/tablesPEX.csv;
+  iconv -f IBM-1047 -t ISO8859-1 $GERS_GIT_REPO_DIR/$PEX_REPO/TABLE/MACRND.csv  > $GERS_GIT_REPO_DIR/$PEX_REPO/TABLE_A/MACRND.csv;
+  iconv -f IBM-1047 -t ISO8859-1 $GERS_GIT_REPO_DIR/$PEX_REPO/TABLE/PGMRND.csv  > $GERS_GIT_REPO_DIR/$PEX_REPO/TABLE_A/PGMRND.csv;
 
-  java -jar $GERS_RCA_JAR_DIR/ftl2jcl-latest.jar COPYPEX $GERS_GIT_REPO_DIR/$PEX_REPO/TABLE/tablesPEX ;
+  java -jar $GERS_RCA_JAR_DIR/ftl2jcl-latest.jar COPYPEX $GERS_GIT_REPO_DIR/$PEX_REPO/TABLE_A/tablesPEX ;
   exitIfError;
 fi 
 #
 # Create build JCL from templates
 #  -- Generate for PE 
-java -jar $GERS_RCA_JAR_DIR/ftl2jcl-latest.jar BUILDPE $GERS_GIT_REPO_DIR/$PE_REPO/TABLE/tablesPE;
+java -jar $GERS_RCA_JAR_DIR/ftl2jcl-latest.jar BUILDPE $GERS_GIT_REPO_DIR/$PE_REPO/TABLE_A/tablesPE;
 exitIfError;
 #  -- Generate for PEX, if required
 if  [ "$GERS_INCLUDE_PEX" == "Y" ]; then 
-  java -jar $GERS_RCA_JAR_DIR/ftl2jcl-latest.jar BUILDPEX $GERS_GIT_REPO_DIR/$PEX_REPO/TABLE/tablesPEX;
+  java -jar $GERS_RCA_JAR_DIR/ftl2jcl-latest.jar BUILDPEX $GERS_GIT_REPO_DIR/$PEX_REPO/TABLE_A/tablesPEX;
   exitIfError;
   ConcatJCL BIG.JCL BUILDPE.jcl BUILDPEX.jcl;
 fi 
