4c80084fbe561dea3082bfa4af8a85cc60909040
neilbeesley <neil.beesley@ibm.com>
2025-05-09 14:05:16 -0500

    Deal with failed assemble link
    Signed-off-by: neilbeesley <neil.beesley@ibm.com>

--- SH/SubBuild.sh
+++ SH/SubBuild.sh
@@ -14,6 +14,12 @@ echo "$(date) ${BASH_SOURCE##*/} JobID: $GERS_JOBID" ;
 exitIfError;
 echo "$(date) ${BASH_SOURCE##*/} Job complete: $GERS_JOBID" ;
 echo "$(date) ${BASH_SOURCE##*/} Job statusRC: $GERS_JOBSTATUS" ;
+
+if  [ "$GERS_JOBSTATUS" == "LT8" ]; then
+  echo "(date) ${BASH_SOURCE##*/} Assemble and Link completed RC 4 or better";
+else
+  echo "(date) ${BASH_SOURCE##*/} Assemble and Link failed -- process terminated (see job output)";
+  exit 1;
 }
 
 exitIfError() {
