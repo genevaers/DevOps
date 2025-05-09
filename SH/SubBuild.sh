951c3f8ee23ddf8a15ac327f5f61b7072295ea74
neilbeesley <neil.beesley@ibm.com>
2025-05-09 16:49:41 -0500

    typo
    Signed-off-by: neilbeesley <neil.beesley@ibm.com>

--- SH/SubBuild.sh
+++ SH/SubBuild.sh
@@ -16,9 +16,9 @@ echo "$(date) ${BASH_SOURCE##*/} Job complete: $GERS_JOBID" ;
 echo "$(date) ${BASH_SOURCE##*/} Job statusRC: $GERS_JOBSTATUS" ;
 
 if  [ "$GERS_JOBSTATUS" == "LT8" ]; then
-  echo "(date) ${BASH_SOURCE##*/} Assemble and Link completed RC 4 or better";
+  echo "$(date) ${BASH_SOURCE##*/} Assemble and Link completed RC 4 or better";
 else
-  echo "(date) ${BASH_SOURCE##*/} Assemble and Link failed -- process terminated (see job output)";
+  echo "$(date) ${BASH_SOURCE##*/} Assemble and Link failed -- process terminated (see job output)";
   exit 1;
 fi
 }
