64d928bf799e74d5ef6baadd940c34fafd6ee20a
neilbeesley <neil.beesley@ibm.com>
2025-09-16 09:49:20 -0500

    Revert my change for EBCDIC
    Signed-off-by: neilbeesley <neil.beesley@ibm.com>

--- FTL2JCL/src/main/java/org/genevaers/ftl2jcl/CommandLineHandler.java
+++ FTL2JCL/src/main/java/org/genevaers/ftl2jcl/CommandLineHandler.java
@@ -22,6 +22,8 @@ import java.io.FileWriter;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
