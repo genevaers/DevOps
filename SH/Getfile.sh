5eb0784ade42f0051f80c9a9901765ea4ee25f24
neilbeesley <neil.beesley@ibm.com>
2026-06-25 14:02:51 -0500

    More getfile
    Signed-off-by: neilbeesley <neil.beesley@ibm.com>

--- SH/Getfile.sh
+++ SH/Getfile.sh
@@ -30,19 +30,30 @@ if [ -z "$GERS_FILESEQN.$GERS_FILEMLLQ" ] || [[ "$GERS_FILESEQN.$GERS_FILEMLLQ" 
   exit 1;
 fi
 
-if [ "$GERS_FILETYPE" = "PDS" ] || [ "$GERS_FILETYPE" = "PS" ]; then
+if [ "$GERS_FILETYPE" = "PDS" ] || [ "$GERS_FILETYPE" = "PS" ] || [ "$GERS_FILETYPE" = "TRS" ]; then
   echo "$(date) ${BASH_SOURCE##*/} Retrieving $GERS_FILETYPE file $GERS_FILESEQN.$GERS_FILEMLLQ from server $GERS_SERVERID for $GERS_SECUREID";
   if [ -z "$GERS_SECUREID" ] || [[ "$GERS_SECUREID" = "" ]]; then
     echo "$(date) ${BASH_SOURCE##*/} Default file size 100 cylinders";
   else
     echo "$(date) ${BASH_SOURCE##*/} Override file size of $GERS_FILECYLS cylinders provided";
   fi
+
   echo "$(date) ${BASH_SOURCE##*/} Following temporary files will be used/overwritted when retrieving data (exit if needed):";
   echo "$(date) ${BASH_SOURCE##*/}   &SYSUID..TRANSFER.TRS";
-  echo "$(date) ${BASH_SOURCE##*/}   &SYSUID..TRANSFER.PDS";
-  echo "$(date) ${BASH_SOURCE##*/}   &SYSUID..TRANSFER.SEQ";
+
+  # cases for dataset types
+  if [ "$GERS_FILETYPE" = "PDS" ] ; then
+    echo "$(date) ${BASH_SOURCE##*/}   &SYSUID..TRANSFER.PDS";
+  fi
+  if [ "$GERS_FILETYPE" = "PS" ] ; then
+    echo "$(date) ${BASH_SOURCE##*/}   &SYSUID..TRANSFER.SEQ";
+  fi
+  if [ "$GERS_FILETYPE" = "TRS" ] ; then
+    echo "$(date) ${BASH_SOURCE##*/} No UNTERSE has been requested (e.g. used for DB2 unload files)";
+  fi
+
 else
-  echo "$(date) ${BASH_SOURCE##*/} FILETYPE of ' $GERS_FILETYPE ' given. Either PDS or PS must be specified";
+  echo "$(date) ${BASH_SOURCE##*/} FILETYPE of ' $GERS_FILETYPE ' given. Either PDS, PS, or TRS (raw) must be specified";
   exit 1;
 fi
 
@@ -56,11 +67,11 @@ mkdir prep;
 # tailor JCL
 FILENAME=$GERS_FILESEQN.$GERS_FILEMLLQ;
 # 7 times as much space for unpacked file
-EXPCYL=$(( GERS_FILECYLS * 7 ))
+EXPCYL=$(( GERS_FILECYLS * 7 ));
 
 mycmdstr1='s/&$FILENM.'/${FILENAME}/'g';
 mycmdstr2='s/&$FILECY.'/${GERS_FILECYLS}/'g';
-mycmdstr3='s/&$RUNPTH.'/${GERS_RUNPATH}/'g';
+mycmdstr3='s|&$RUNPTH.'|${$GERS_GIT_REPO_DIR}|'g';
 mycmdstr4='s/&$FILECY.'/${EXPCYL}/'g';
 
 echo "cmd1: $mycmdstr1";
@@ -88,9 +99,9 @@ iconv -f ISO8859-1 -t IBM-1047 prep/tmp5 > prep/UNTERS3.jcl;
 chtag -r prep/UNTERS2.jcl;
 
 # append completion status job step
-cat prep/UNTERS1.jcl ../JCL/UNTERSEDONE.jcl > prep/UNTERSE1.jcl
-cat prep/UNTERS2.jcl ../JCL/UNTERSEDONE.jcl > prep/UNTERSE2.jcl
-cat prep/UNTERS3.jcl ../JCL/UNTERSEDONE.jcl > prep/UNTERSE3.jcl
+cat prep/UNTERS1.jcl ../JCL/UNTERSEDONE.jcl > prep/UNTERSE1.jcl;
+cat prep/UNTERS2.jcl ../JCL/UNTERSEDONE.jcl > prep/UNTERSE2.jcl;
+cat prep/UNTERS3.jcl ../JCL/UNTERSEDONE.jcl > prep/UNTERSE3.jcl;
 
 # run UNTERSE1 to copy USS file to MVS dataset
 echo "$(date) ${BASH_SOURCE##*/} Submit generated JCL to copy USS file to MVS dataset";
@@ -110,12 +121,14 @@ if [ "$GERS_FILETYPE" = "PDS" ]; then
   exitIfError;
   echo "$(date) ${BASH_SOURCE##*/} Job complete: $GERS_JOBID" ;
   echo "$(date) ${BASH_SOURCE##*/} Job statusRC: $GERS_JOBSTATUS" ;
-else
-# run UNTERSE3 to perform TRSMAIN to UNPACK PS dataset
+fi
+
+if [ "$GERS_FILETYPE" = "PS" ] ; then
+# run UNTERSE3 to perform TRSMAIN to UNPACK PS dataset: could take 10 minutes
   echo "$(date) ${BASH_SOURCE##*/} Submit generated JCL to UNTERSE MVS sequential dataset";
   . ./JobSubmitter.sh 'prep/UNTERSE3.jcl' untersedone  1>> $err_log;
   echo "$(date) ${BASH_SOURCE##*/} JobID: $GERS_JOBID" ;
-  . ./JobWaiter.sh 120 untersedone  1>> $err_log ;
+  . ./JobWaiter.sh 600 untersedone  1>> $err_log ;
   exitIfError;
   echo "$(date) ${BASH_SOURCE##*/} Job complete: $GERS_JOBID" ;
   echo "$(date) ${BASH_SOURCE##*/} Job statusRC: $GERS_JOBSTATUS" ;
